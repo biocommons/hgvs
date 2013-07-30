c9559a13e90eb059a01181047609554f6378ecd8
Reece Hart <reecehart@gmail.com>
2013-07-29 22:32:23 -0700

    added more location tests

--- tests/test_hgvs_location.py
+++ tests/test_hgvs_location.py
@@ -9,17 +9,41 @@ class Test_Position(unittest.TestCase):
             self.assertEqual( hgvs.location.Position(-1) )
 
     def test_CDSPosition(self):
-        cdsp = hgvs.location.CDSPosition(5,10)
+        cdsp = hgvs.location.CDSPosition(5)
         self.assertEqual( cdsp.base, 5 )
-        self.assertEqual( cdsp.offset, 10 )
+        self.assertEqual( cdsp.offset, 0 )
+        self.assertEqual( str(cdsp), '5' )
+
+        cdsp = hgvs.location.CDSPosition(5,6)
+        self.assertEqual( cdsp.base, 5 )
+        self.assertEqual( cdsp.offset, 6 )
+        self.assertEqual( str(cdsp), '5+6' )
+
+        cdsp = hgvs.location.CDSPosition(5,-7)
+        self.assertEqual( cdsp.base, 5 )
+        self.assertEqual( cdsp.offset, -7 )
+        self.assertEqual( str(cdsp), '5-7' )
+
+        cdsp = hgvs.location.CDSPosition(0,-7)
+        self.assertEqual( cdsp.base, 0 )
+        self.assertEqual( cdsp.offset, -7 )
+        self.assertEqual( str(cdsp), '-7' )
+
+        cdsp = hgvs.location.CDSPosition(0,+7)
+        self.assertEqual( cdsp.base, 0 )
+        self.assertEqual( cdsp.offset, +7 )
+        self.assertEqual( str(cdsp), '*7' )
+
 
     def test_Range(self):
-        cdsr = hgvs.location.Range( hgvs.location.CDSPosition(base=55,offset=-10),
-                                    hgvs.location.CDSPosition(base=56,offset=-8) )
-        self.assertEqual( cdsr.start.base, 55 )
-        self.assertEqual( cdsr.start.offset, -10 )
+        cdsr = hgvs.location.Range( hgvs.location.CDSPosition(base=12,offset=+34),
+                                    hgvs.location.CDSPosition(base=56,offset=-78) )
+        self.assertEqual( cdsr.start.base, 12 )
+        self.assertEqual( cdsr.start.offset, 34 )
         self.assertEqual( cdsr.end.base, 56 )
-        self.assertEqual( cdsr.end.offset, -8 )
+        self.assertEqual( cdsr.end.offset, -78 )
+        self.assertEqual( str(cdsr), '12+34_56-78' )
+
 
 if __name__ == '__main__':
     unittest.main()
