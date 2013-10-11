ed2a6fee47801af59bc7ec9a7ba4f60f40991bdf
Reece Hart <reecehart@gmail.com>
2013-10-10 21:56:33 -0700

    updated variant and added test

--- hgvs/variant.py
+++ hgvs/variant.py
@@ -1,11 +1,11 @@
 import recordtype
 
-class Variant( recordtype.recordtype('Variant', ['seqref','type','pos','edit'], default=None) ):
+class Variant( recordtype.recordtype('Variant', ['seqref','type','posedits']) ):
     """
     represents a basic HGVS variant.  The only requirement is that each
     component can be stringified; for example, passing pos as either a string
     or an hgvs.location.CDSInterval (for example) are both intended uses
     """
-
+    
     def __str__(self):
-        return '{self.seqref}:{self.type}.{self.pos}{self.edit}'.format(self=self)
+        return '{self.seqref}:{self.type}.{self.posedits}'.format(self=self)
