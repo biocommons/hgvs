23feafe78be5eb9d0597ac31007ed34adeea166f
Reece Hart <reecehart@gmail.com>
2013-10-11 08:58:26 -0700

    added HGVSPosition (aka HGVS Lite)

--- hgvs/parser.py
+++ hgvs/parser.py
@@ -3,6 +3,7 @@ import os
 import parsley
 
 import hgvs.edit
+import hgvs.hgvsposition
 import hgvs.location
 import hgvs.posedit
 import hgvs.variant
@@ -25,12 +26,33 @@ class Parser(object):
             
             'PosEdit': hgvs.posedit.PosEdit,
 
+            'HGVSPosition': hgvs.hgvsposition.HGVSPosition,
             'Variant': hgvs.variant.Variant,
             })
+
+        self.rules = [ m.replace('rule_','')
+                       for m in dir(self._grammar._grammarClass)
+                       if m.startswith('rule_') ]
+
+    def __getattr__(self,name):
+        # http://docs.python.org/2/reference/datamodel.html#object.__getattr__
+        """
+        attempt to call name as a grammar rule
         
+        This is challenging because the call structure is self._grammar(s).name(),
+        where s is the string to parse, which isn't provided to __getattr__.
+        Notice that we need to call name() on an object that doesn't exist yet.
+
+        This function returns a function that takes a string and returns the parsing result.
+        """
+        return lambda s: self._grammar(s).__getattr__(name)()
+        
+
     def parse(self,variant):
         return self._grammar(variant).hgvs_variant()
         #try:
         #except Exception as e:
         #    raise HGVSParseError('{variant}: parsing raised {type} ({e.message})'.format(
         #        variant = variant, type = type(e), e = e))
+
+        
