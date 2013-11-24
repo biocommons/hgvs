332ecfc79bcad76b477e72810c02e6625040b129
Reece Hart <reecehart@gmail.com>
2013-11-24 15:08:58 -0800

    hgvs.utils: handle case when aa string is None

--- hgvs/utils.py
+++ hgvs/utils.py
@@ -14,12 +14,12 @@ lowercase = 'abcdefghijklmnopqrstuvwxyz'   # rather than import string
 
 def aa3_to_aa1(s):
     "convert string of 3-letter amino acids to 1-letter amino acids"
-    return ''.join([aa3_to_aa1_lut[aa3] for aa3 in
-                    [ s[i:i+3] for i in range(0,len(s),3) ]])
+    return None if s is None else  ''.join([aa3_to_aa1_lut[aa3] for aa3 in
+                                            [ s[i:i+3] for i in range(0,len(s),3) ]])
 
 def aa1_to_aa3(s):
     "convert string of 1-letter amino acids to 3-letter amino acids"
-    return ''.join([aa1_to_aa3_lut[aa1] for aa1 in s])
+    return None if s is None else ''.join([aa1_to_aa3_lut[aa1] for aa1 in s])
 
 def aa_to_aa1(s):
     "coerce string of 1- or 3-letter amino acids to 1-letter"
@@ -32,6 +32,7 @@ def aa_to_aa3(s):
 def __looks_like_aa3_p(s):
     "string looks like a 3-letter AA string"
     return (
-        (len(s) % 3 == 0)
+        s is not None
+        and (len(s) % 3 == 0)
         and (len(s) == 0 or s[1] in lowercase)
         )
