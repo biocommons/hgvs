754f2aa392e0f9f129ccf40e22da92b947512c26
Rudy Rico <rudy.rico@invitae.com>
2013-11-27 11:52:12 -0800

    Incorporate aa util and extend interval class (for test data); convert code to produce SequenceVariant objects for hgvs c to p.   Also hacked in a way to handle p.= into the grammar (should be reviewed before merge).

--- hgvs/translators/hgvs_c_to_p.py
+++ hgvs/translators/hgvs_c_to_p.py
@@ -7,7 +7,7 @@ from Bio.Seq import Seq
 
 import hgvs.edit
 import hgvs.parser
-import hgvs.translators.utils.aminoacidutils as aminoacidutils
+import hgvs.utils
 import hgvs.translators.utils.proteincomparer as proteincomparer
 import hgvs.translators.utils.variantinserter as variantinserter
 
@@ -33,13 +33,13 @@ class HgvsCToP(object):
         """
 
         # translate hgvsc
-        var = self.parser.hgvs_variant(hgvsc)
+        var = self.parser.parse_hgvs_variant(hgvsc)
         if DBG:
             print "START DEBUG"
             print hgvsc
 
         # get transcript from datasource & convert to AA
-        transcript_data = self.datasource.get_sequence(var.seqref)
+        transcript_data = self._setup_transcript_data(var.seqref)
 
         transcript_dna_cds = \
             Seq(transcript_data['transcript_sequence'][transcript_data['cds_start'] - 1:transcript_data['cds_stop']])
@@ -55,10 +55,10 @@ class HgvsCToP(object):
 
         # perform comparison to get hgvs tag
         comparer = proteincomparer.ProteinComparer(transcript_data['AA_sequence'],
-                                                variant_data[0]['AA_sequence'], variant_data[0]['frameshift_start'])
-        vars = comparer.compare()
-        hgvsp = self._convert_all_to_hgvsp(vars, transcript_data['AA_sequence'], variant_data[0]['AA_sequence'],
-                                           variant_data[0]['protein_accession'])
+                                                variant_data[0].aa_sequence, variant_data[0].frameshift_start)
+        hgvsp_no_acc = comparer.compare()
+
+        hgvsp = self._add_accession(hgvsp_no_acc, variant_data[0].protein_accession)
 
         if DBG:
             print "END DEBUG"
@@ -69,142 +69,30 @@ class HgvsCToP(object):
     # internal methods
     #
 
-    def _convert_all_to_hgvsp(self, variants, ref_seq, alt_seq, accession):
+    def _setup_transcript_data(self, ac):
+        """wrapper to convert transcript data from external source into common form
+        :param ac accession #
+        :type str
+        :return transcript info
+        :type dict
+        """
+        # TODO - take whatever form you get the input from & convert to a uniform format
+        # TODO - handle what UTA returns & convert test inputs to mimic that
+        transcript_data = self.datasource.fetch_transcript_exons(ac)
+        return transcript_data
 
-
-        # convert variant to hgvs p. format
-        # p.[(<variant1>, <variant2,...>)]
-        hgvsps = [self._convert_AA_variant_to_hgvs(variant, ref_seq, alt_seq) for variant in variants]
+    def _add_accession(self, hgvsps, acc):
+        """Helper to add protein accession to hgvsp
+        """
+        for hgvsp in hgvsps:
+            hgvsp.ac = acc
 
         if len(hgvsps) > 1:
-            result = "{}:p.[({})]".format(accession, ';'.join(hgvsps))
-        elif len(hgvsps) == 1:
-            result = "{}:p.({})".format(accession, hgvsps[0])
+            raise NotImplementedError("Can only handle single variants")
         else:
-            result = "{}:p.(=)".format(accession)
+            hgvsp = hgvsps[0]
 
-        if DBG:
-            print hgvsps
-            print result
-
-        return result
-
-
-    def _convert_AA_variant_to_hgvs(self, variant, ref_seq, alt_seq):
-        """Convert AA variant to an hgvs representation
-        :param variant: contains start, del, and ins
-        :type dict
-        :param ref_seq: AA reference
-        :type string
-        :param alt_seq: AA reference with variants
-        :type string
-        :return hgvs string
-        """
-        start = variant['start']
-        insertion = variant['ins']
-        deletion = variant['del']
-
-        is_frameshift = insertion and deletion and deletion[-1] == '*'
-
-        # frameshift
-        if is_frameshift:
-            delstart3 = aminoacidutils.convert_AA_1_to_3(deletion[0])
-            insstart3 = aminoacidutils.convert_AA_1_to_3(insertion[0])
-            try:
-                new_stop = str(insertion.index("*") + 1)    # start w/ 1st change; ends w/ * (inclusive)
-            except ValueError:
-                new_stop = "?"
-
-            if new_stop != "1":
-                tag = "{}{}{}fs*{}".format(delstart3, start, insstart3, new_stop)
-            else:   # frameshift introduced stop codon at variant position
-                tag = "{}{}*".format(delstart3, start)
-
-        # no frameshift
-        else:
-
-            deletion3 = aminoacidutils.convert_AA_1_to_3(deletion)
-            insertion3 = aminoacidutils.convert_AA_1_to_3(insertion)
-
-            if len(insertion) == len(deletion) == 1:        # substitution
-                tag = '{}{}{}'.format(deletion3, start, insertion3)
-
-            elif len(deletion) > 0:
-                end = start + len(deletion) - 1                         # delins OR deletion
-                delstart3 = aminoacidutils.convert_AA_1_to_3(deletion[0])
-                delend3 = aminoacidutils.convert_AA_1_to_3(deletion[-1])
-
-                if len(insertion) > 0:                                  # delins
-                    tag = '{}{}_{}{}delins{}'.format(delstart3, start, delend3, end, insertion3) if end > start \
-                        else '{}{}delins{}'.format(deletion3, start, insertion3)
-                else:                                                   # deletion
-                    if len(deletion) + start == len(ref_seq): # stop codon introduced at variant position
-                        tag = '{}{}*'.format(delstart3, start)
-                    else:
-                        tag = '{}{}_{}{}del'.format(delstart3, start, delend3, end) if end > start \
-                            else '{}{}del'.format(deletion3, start)
-
-            elif len(deletion) == 0:                                    # insertion OR duplication
-
-                is_dup, variant_start = self._check_if_ins_is_dup(start, insertion, ref_seq)
-
-                if is_dup:                                              # duplication
-                    dupstart3 = aminoacidutils.convert_AA_1_to_3(insertion[0])
-                    dupend3 = aminoacidutils.convert_AA_1_to_3(insertion[-1])
-                    tag = '{}{}_{}{}dup'.format(dupstart3, variant_start, dupend3, variant_start + len(insertion) - 1) \
-                        if len(insertion) > 1 else '{}{}dup'.format(dupstart3, variant_start)
-
-
-                else:                                                   # insertion
-                    if start == len(ref_seq):                           # extension
-                        len_ext = len(insertion) - 1  # don't include the former stop codon
-                        subst_at_stop_codon = aminoacidutils.convert_AA_1_to_3(insertion[-1])
-                        tag = '*{}{}ext*{}'.format(start, subst_at_stop_codon, len_ext)
-                    else:                                               # insertion
-                        ref_start3 = aminoacidutils.convert_AA_1_to_3(ref_seq[start - 1])
-                        ref_end3 = aminoacidutils.convert_AA_1_to_3(ref_seq[start])
-                        tag = '{}{}_{}{}ins{}'.format(ref_start3, start, ref_end3, start + 1, insertion3)
-
-            else: # should never get here
-                raise ValueError("unexpected variant: {}".format(variant))
-
-        return tag
-
-
-    def _check_if_ins_is_dup(self, start, insertion, ref_seq):
-        """Helper to identify an insertion as a duplicate
-        """
-
-        is_dup = False  # assume no
-        variant_start = -1
-
-        if len(insertion) == 1:  # since difflib may match 1-AA dups before or after, need to check both
-            ref_prev = [ref_seq[start - 2]]
-            ref_next = [ref_seq[start - 1]]
-            if DBG:
-                print start
-                print insertion
-                print ref_prev, ref_next
-            if insertion == ref_prev or insertion == ref_next:
-                is_dup = True
-                variant_start = start - 1 if insertion == ref_prev else start
-
-        else:
-            # figure out the candidate ref positions that should match if the insertion is a dup
-            dup_candidate_start = start - len(insertion) - 1
-            dup_candidate_end = dup_candidate_start + len(insertion)
-            dup_candidate = list(ref_seq[dup_candidate_start: dup_candidate_end])
-
-            if DBG:
-                print dup_candidate_start, dup_candidate_end
-                pprint.pprint(insertion)
-                pprint.pprint(dup_candidate)
-
-            if insertion == dup_candidate:
-                is_dup = True
-                variant_start = dup_candidate_start + 1
-
-        return is_dup, variant_start
+        return hgvsp
 
 
 
--- hgvs/translators/utils/proteincomparer.py
+++ hgvs/translators/utils/proteincomparer.py
@@ -6,23 +6,21 @@ import collections
 import difflib
 import pprint
 
-import hgvs.edit as edit
-import hgvs.location as location
-import hgvs.translators.utils.aminoacidutils as aminoacidutils
-
+import hgvs.edit
+import hgvs.location
+import hgvs.posedit
+import hgvs.utils
+import hgvs.variant
 
 DBG = True
 
 class ProteinComparer(object):
 
-    def __init__(self, ref_seq, alt_seq, frameshift_start = -1, force_frameshift=True):
+    def __init__(self, ref_seq, alt_seq, frameshift_start = None):
         """Constructor
 
         The normal difflib comparison may identify islands of matches in a frameshift
         which will end up being reported as multiple variants.
-
-        The force_frameshift param is used to tell the app to treat that frameshift as the last variant,
-        rather than just use the results of the diff.
 
         :param ref_seq: protein reference sequence
         :type str
@@ -30,13 +28,10 @@ class ProteinComparer(object):
         :type str
         :param frameshift_start: index n protein (1-based) where a frameshift should affect all downstream bases
         :type int
-        :param force_frameshift: treat a variant which would shift all subsequent bases as the terminal frameshift
-        :type bool
         """
         self._ref_seq = ref_seq
         self._alt_seq = alt_seq
         self._frameshift_start = frameshift_start
-        self._force_frameshift = force_frameshift
 
     def compare(self):
         """Compare two amino acid sequences; generate an hgvs tag from the output
@@ -48,12 +43,6 @@ class ProteinComparer(object):
         indel_map = {'+':'ins', '-':'del'}
 
         diff_list = difflib.Differ().compare(self._ref_seq, self._alt_seq)
-
-        # initial check for frameshift
-        # ref_stop = len(self._ref_seq) - 1
-        # alt_stop = self._alt_seq.find("*")
-        # has_frameshift = alt_stop != -1 and alt_stop != ref_stop
-
 
         # walk through the difflib string list and summarize variants as they are encountered
         ref_index = 1
@@ -79,12 +68,11 @@ class ProteinComparer(object):
                     current_var = collections.defaultdict(list)
                     current_var['start'] = ref_index
 
-                    if self._force_frameshift:
-                        if self._frameshift_start != -1 and current_var['start'] >= self._frameshift_start:
-                            current_var = self._force_variant_to_frameshift(current_var, ref_index, alt_index)
-                            variants.append(current_var)
-                            in_variant = False
-                            break
+                    if self._frameshift_start and current_var['start'] >= self._frameshift_start:
+                        current_var = self._force_variant_to_frameshift(current_var, ref_index, alt_index)
+                        variants.append(current_var)
+                        in_variant = False
+                        break
 
                 current_var[indel_map[change]].append(base)
                 if change == '-':
@@ -100,9 +88,12 @@ class ProteinComparer(object):
             print(debug_item)
             pprint.pprint(variants)
 
-        #sequence_variants = [self._convert_to_sequence_variants(x) for x in variants]
+        if variants:
+            sequence_variants = [self._convert_to_sequence_variants(x, 'TEST') for x in variants]
+        else:    # create a "silent" variant
+            sequence_variants = [self._create_variant('', '', '', '')]
 
-        return variants
+        return sequence_variants
 
     #
     # internal methods
@@ -124,127 +115,179 @@ class ProteinComparer(object):
         current_variant['ins'] = list(self._alt_seq[alt_index - 1:])
         return current_variant
 
-    # def _convert_to_sequence_variants(self, variant):
-    #     """Convert AA variant (as a dictionary) to SequenceVariant
-    #     :param variant: contains start, del, and ins
-    #     :type dict
-    #     :return SequenceVariant
-    #     """
-    #     start = variant['start']
-    #     insertion = variant['ins']
-    #     deletion = variant['del']
-    #
-    #     is_frameshift = insertion and deletion and deletion[-1] == '*'
-    #
-    #     # frameshift
-    #     if is_frameshift:
-    #         delstart3 = aminoacidutils.convert_AA_1_to_3(deletion[0])
-    #         insstart3 = aminoacidutils.convert_AA_1_to_3(insertion[0])
-    #         try:
-    #             new_stop = str(insertion.index("*") + 1)    # start w/ 1st change; ends w/ * (inclusive)
-    #         except ValueError:
-    #             new_stop = "?"
-    #
-    #         if new_stop != "1":
-    #             tag = "{}{}{}fs*{}".format(delstart3, start, insstart3, new_stop)
-    #         else:   # frameshift introduced stop codon at variant position
-    #             tag = "{}{}*".format(delstart3, start)
-    #
-    #     # no frameshift
-    #     else:
-    #
-    #         deletion3 = aminoacidutils.convert_AA_1_to_3(deletion)
-    #         insertion3 = aminoacidutils.convert_AA_1_to_3(insertion)
-    #
-    #         if len(insertion) == len(deletion) == 1:        # substitution
-    #             tag = '{}{}{}'.format(deletion3, start, insertion3)
-    #             ref_alt = edit.RefAlt(ref=None, alt=insertion3)
-    #             aapos_start = location.AAPosition(pos=)
-    #
-    #         elif len(deletion) > 0:
-    #             end = start + len(deletion) - 1                         # delins OR deletion
-    #             delstart3 = aminoacidutils.convert_AA_1_to_3(deletion[0])
-    #             delend3 = aminoacidutils.convert_AA_1_to_3(deletion[-1])
-    #
-    #             if len(insertion) > 0:                                  # delins
-    #                 tag = '{}{}_{}{}delins{}'.format(delstart3, start, delend3, end, insertion3) if end > start \
-    #                     else '{}{}delins{}'.format(deletion3, start, insertion3)
-    #             else:                                                   # deletion
-    #                 if len(deletion) + start == len(self._ref_seq): # stop codon introduced at variant position
-    #                     tag = '{}{}*'.format(delstart3, start)
-    #                 else:
-    #                     tag = '{}{}_{}{}del'.format(delstart3, start, delend3, end) if end > start \
-    #                         else '{}{}del'.format(deletion3, start)
-    #
-    #         elif len(deletion) == 0:                                    # insertion OR duplication
-    #
-    #             is_dup, variant_start = self._check_if_ins_is_dup(start, insertion, self._ref_seq)
-    #
-    #             if is_dup:                                              # duplication
-    #                 dupstart3 = aminoacidutils.convert_AA_1_to_3(insertion[0])
-    #                 dupend3 = aminoacidutils.convert_AA_1_to_3(insertion[-1])
-    #                 tag = '{}{}_{}{}dup'.format(dupstart3, variant_start, dupend3, variant_start + len(insertion) - 1) \
-    #                     if len(insertion) > 1 else '{}{}dup'.format(dupstart3, variant_start)
-    #
-    #
-    #             else:                                                   # insertion
-    #                 if start == len(self._ref_seq):                           # extension
-    #                     len_ext = len(insertion) - 1  # don't include the former stop codon
-    #                     subst_at_stop_codon = aminoacidutils.convert_AA_1_to_3(insertion[-1])
-    #                     tag = '*{}{}ext*{}'.format(start, subst_at_stop_codon, len_ext)
-    #                 else:                                               # insertion
-    #                     ref_start3 = aminoacidutils.convert_AA_1_to_3(self._ref_seq[start - 1])
-    #                     ref_end3 = aminoacidutils.convert_AA_1_to_3(self._ref_seq[start])
-    #                     tag = '{}{}_{}{}ins{}'.format(ref_start3, start, ref_end3, start + 1, insertion3)
-    #
-    #         else: # should never get here
-    #             raise ValueError("unexpected variant: {}".format(variant))
-    #
-    #     return tag
-    #
-    #
-    # def _check_if_ins_is_dup(self, start, insertion, ref_seq):
-    #     """Helper to identify an insertion as a duplicate
-    #     :param start 1-based start position in reference sequence
-    #     :type int
-    #     :param insertion amino acid insertion sequence
-    #     :type list
-    #     :param ref_seq amino acid reference sequence
-    #     :type str
-    #     :return (is duplicate [boolean], variant start [-1 if not a duplicate)
-    #     :type tuple
-    #     """
-    #
-    #     is_dup = False  # assume no
-    #     variant_start = -1
-    #
-    #     if len(insertion) == 1:  # since difflib may match single AA dups before or after, need to check both
-    #         ref_prev = [ref_seq[start - 2]]
-    #         ref_next = [ref_seq[start - 1]]
-    #         if DBG:
-    #             print start
-    #             print insertion
-    #             print ref_prev, ref_next
-    #         if insertion == ref_prev or insertion == ref_next:
-    #             is_dup = True
-    #             variant_start = start - 1 if insertion == ref_prev else start
-    #
-    #     else:
-    #         # figure out the candidate ref positions that should match if the insertion is a dup
-    #         dup_candidate_start = start - len(insertion) - 1
-    #         dup_candidate_end = dup_candidate_start + len(insertion)
-    #         dup_candidate = list(ref_seq[dup_candidate_start: dup_candidate_end])
-    #
-    #         if DBG:
-    #             print dup_candidate_start, dup_candidate_end
-    #             pprint.pprint(insertion)
-    #             pprint.pprint(dup_candidate)
-    #
-    #         if insertion == dup_candidate:
-    #             is_dup = True
-    #             variant_start = dup_candidate_start + 1
-    #
-    #     return is_dup, variant_start
+
+
+
+    def _convert_all_to_hgvsp(self, variants, ref_seq, accession):
+
+
+        # convert variant to hgvs p. format
+        # p.[(<variant1>, <variant2,...>)]
+        hgvsps = [self._convert_AA_variant_to_hgvs(variant, ref_seq) for variant in variants]
+
+        if len(hgvsps) > 1:
+            result = "{}:p.[({})]".format(accession, ';'.join(hgvsps))
+        elif len(hgvsps) == 1:
+            result = "{}:p.({})".format(accession, hgvsps[0])
+        else:
+            result = "{}:p.(=)".format(accession)
+
+        if DBG:
+            print hgvsps
+            print result
+
+        return result
+
+
+    def _convert_to_sequence_variants(self, variant, acc):
+        """Convert AA variant to an hgvs representation
+        :param variant: contains start, del, and ins
+        :type dict
+        :param acc: protein accession
+        :type str
+        :return hgvs string
+        """
+        start = variant['start']
+        insertion = ''.join(variant['ins'])
+        deletion = ''.join(variant['del'])
+
+        is_frameshift = insertion and deletion and deletion[-1] == '*'
+
+        # defaults
+        is_dup = False  # assume not dup
+        fs = None
+
+        if is_frameshift:                                               # frameshift
+            aa_start = aa_end = hgvs.location.AAPosition(pos=start, aa=deletion[0])
+            ref = ''
+
+            try:
+                new_stop = str(insertion.index("*") + 1)    # start w/ 1st change; ends w/ * (inclusive)
+            except ValueError:
+                new_stop = "?"
+
+            if new_stop != "1":
+                alt = insertion[0]
+                fs = 'fs*{}'.format(new_stop)
+
+            else:   # frameshift introduced stop codon at variant position
+                alt = '*'
+
+        else:                                                           # no frameshift
+            if len(insertion) == len(deletion) == 1:                    # substitution
+                aa_start = aa_end = hgvs.location.AAPosition(pos=start, aa=deletion)
+                ref = ''
+                alt = insertion
+
+            elif len(deletion) > 0:                                     # delins OR deletion
+                ref = ''
+                aa_start = hgvs.location.AAPosition(pos=start, aa=deletion[0])
+
+                end = start + len(deletion) - 1
+                if len(insertion) > 0:                                  # delins
+                    if end > start:
+                        aa_end =  hgvs.location.AAPosition(pos=end, aa=deletion[-1])
+                    else:
+                        aa_end = aa_start
+                    alt = insertion
+
+                else:                                                   # deletion OR stop codon at variant position
+                    if len(deletion) + start == len(self._ref_seq):     # stop codon at variant position
+                        aa_end = hgvs.location.AAPosition(pos=start, aa=deletion[0])
+                        alt = '*'
+                    else:                                               # deletion
+                        if end > start:
+                            aa_end =  hgvs.location.AAPosition(pos=end, aa=deletion[-1])
+                        else:
+                            aa_end = aa_start
+                        alt = None
+
+            elif len(deletion) == 0:                                    # insertion OR duplication OR extension
+
+                is_dup, dup_start = self._check_if_ins_is_dup(start, insertion, self._ref_seq)
+
+                if is_dup:                                              # duplication
+                    dup_end = dup_start + len(insertion) - 1
+                    aa_start = hgvs.location.AAPosition(pos=dup_start, aa=insertion[0])
+                    aa_end =  hgvs.location.AAPosition(pos=dup_end, aa=insertion[-1])
+                    ref = alt = None
+
+                else:                                                   # insertion OR extension
+                    if start == len(self._ref_seq):                     # extension
+                        len_ext = len(insertion) # don't include the former stop codon
+                        subst_at_stop_codon = insertion[-1]
+
+                        aa_start = aa_end = hgvs.location.AAPosition(pos=start, aa='*')
+                        ref = ''
+                        alt = subst_at_stop_codon
+                        fs ='ext*{}'.format(len_ext)
+
+                    else:                                               # insertion
+                        end = start + 1
+
+                        aa_start = hgvs.location.AAPosition(pos=start, aa=self._ref_seq[start - 1])
+                        aa_end =  hgvs.location.AAPosition(pos=end, aa=self._ref_seq[end - 1])
+                        ref = None
+                        alt = insertion
+
+            else: # should never get here
+                raise ValueError("unexpected variant: {}".format(variant))
+
+        var_seq = self._create_variant(aa_start, aa_end, ref, alt, fs, is_dup)
+
+        if DBG:
+            print "var_seq: {}".format(var_seq)
+
+        return var_seq
+
+
+    def _check_if_ins_is_dup(self, start, insertion, ref_seq):
+        """Helper to identify an insertion as a duplicate
+        """
+        is_dup = False  # assume no
+        variant_start = None
+
+        if len(insertion) == 1:  # since difflib may match 1-AA dups before or after, need to check both
+            ref_prev = ref_seq[start - 2]
+            ref_next = ref_seq[start - 1]
+            if DBG:
+                print start
+                print insertion
+                print ref_prev, ref_next
+            if insertion == ref_prev or insertion == ref_next:
+                is_dup = True
+                variant_start = start - 1 if insertion == ref_prev else start
+
+        else:
+            # figure out the candidate ref positions that should match if the insertion is a dup
+            dup_candidate_start = start - len(insertion) - 1
+            dup_candidate_end = dup_candidate_start + len(insertion)
+            dup_candidate = ref_seq[dup_candidate_start: dup_candidate_end]
+
+            if DBG:
+                print dup_candidate_start, dup_candidate_end
+                pprint.pprint(insertion)
+                pprint.pprint(dup_candidate)
+
+            if insertion == dup_candidate:
+                is_dup = True
+                variant_start = dup_candidate_start + 1
+
+        return is_dup, variant_start
+
+    def _create_variant(self, start, end, ref, alt, fs=None, is_dup=False):
+        """Creates a SequenceVariant object
+        """
+        interval = hgvs.location.Interval(start=start, end=end)
+        if is_dup:
+            edit = hgvs.edit.Dup()
+        else:
+            edit = hgvs.edit.AARefAlt(ref=ref, alt=alt, fs=fs)
+        posedit = hgvs.posedit.PosEdit(interval, edit)
+        var = hgvs.variant.SequenceVariant(None, 'p', posedit)
+
+        return var
+
 
 
 
