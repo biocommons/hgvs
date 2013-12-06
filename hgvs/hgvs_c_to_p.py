98c2c329f6d41dbed89234ba194266f2ba7e1486
Rudy Rico <rudy.rico@invitae.com>
2013-12-06 12:48:12 -0800

    align with developer.rst conventions on naming hgvs variants vs. strings

--- hgvs/hgvs_c_to_p.py
+++ hgvs/hgvs_c_to_p.py
@@ -5,7 +5,7 @@ from Bio.Seq import Seq
 import recordtype
 
 import hgvs.edit
-import hgvs.parser
+#import hgvs.parser
 import hgvs.stopgap
 import hgvs.utils
 import hgvs.utils.altseq_to_hgvsp as altseq_to_hgvsp
@@ -17,14 +17,14 @@ class HgvsCToP(object):
     def __init__(self,db=None,cache_transcripts=False):
         self.db = db
         self.cache_transcripts = cache_transcripts
-        self.hgvs_parser = hgvs.parser.Parser()
+        #self.hgvs_parser = hgvs.parser.Parser()
         self.tm_cache = {}
 
-    def hgvsc_to_hgvsp(self, hgvsc):
+    def hgvsc_to_hgvsp(self, var_c):
         """Convert hgvsc tag to hgvsp tag
 
-        :param hgvsc: hgvsc tag
-        :type str
+        :param var_c: hgvsc tag
+        :type SequenceVariant
         :return hgvsp tag
         :type SequenceVariant
         """
@@ -45,9 +45,7 @@ class HgvsCToP(object):
 
                 # concatenate sequences by exon
                 ord_seq = {x['ord']: x['t_seq_a'] for x in ts_exons}
-                ordered_keys = ord_seq.keys()
-                ordered_keys.sort()
-                transcript_sequence = ''.join(ord_seq[x] for x in ordered_keys)
+                transcript_sequence = ''.join(ord_seq[x] for x in sorted(ord_seq.keys()))
 
                 transcript_dna_cds = Seq(transcript_sequence[cds_start - 1:cds_stop])
                 protein_seq = str(transcript_dna_cds.translate())
@@ -59,25 +57,24 @@ class HgvsCToP(object):
                 return transcript_data
 
 
-        var = self.hgvs_parser.parse_hgvs_variant(hgvsc)
-        reference_data = RefTranscriptData.setup_transcript_data(var.seqref, self.db)
-        builder = altseqbuilder.AltSeqBuilder(var, reference_data)
+        reference_data = RefTranscriptData.setup_transcript_data(var_c.seqref, self.db)
+        builder = altseqbuilder.AltSeqBuilder(var_c, reference_data)
 
         # TODO - handle case where you get 2+ alt sequences back; currently get list of 1 element
         # loop structure implemented to handle this, but doesn't really do anything currently.
         alt_data = builder.build_altseq()
 
-        hgvsps = []
+        var_ps = []
         for alt in alt_data:
             builder = altseq_to_hgvsp.AltSeqToHgvsp(reference_data.aa_sequence,
                                                     alt.aa_sequence,
                                                     reference_data.protein_accession,
                                                     alt.frameshift_start
                                                     )
-            hgvsp = builder.build_hgvsp()
-            hgvsps.append(hgvsp)
+            var_p = builder.build_hgvsp()
+            var_ps.append(var_p)
 
-        hgvsp = hgvsps[0]
+        var_p = var_ps[0]
 
-        return str(hgvsp)
+        return var_p
 
--- tests/test_hgvs_c_to_p.py
+++ tests/test_hgvs_c_to_p.py
@@ -5,139 +5,141 @@ import os
 import unittest
 
 import hgvs.hgvs_c_to_p as hgvs_c_to_p
+import hgvs.parser
 import framework.mock_input_source as mock_input_data_source
+
 
 class TestHgvsCToP(unittest.TestCase):
 
-
-    fn = os.path.join( os.path.dirname(__file__), 'data', 'hgvsc_to_hgvsp_sanity_data.tsv' )
+    fn = os.path.join(os.path.dirname(__file__), 'data', 'hgvsc_to_hgvsp_sanity_data.tsv')
     _datasource = mock_input_data_source.MockInputSource(fn)
     _translator = hgvs_c_to_p.HgvsCToP(_datasource)
+    _parser = hgvs.parser.Parser()
 
     def test_silent(self):
         hgvsc = "NM_999999.1:c.6A>G"
-        expected_hgvsp = "MD5_87c461c4:p.="
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.="
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_substitution(self):
         hgvsc = "NM_999999.1:c.6A>T"
-        expected_hgvsp = "MD5_87c461c4:p.Lys2Asn"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys2Asn"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_substitution_introduces_stop_codon(self):
         hgvsc = "NM_999996.1:c.8C>A"
-        expected_hgvsp = "MD5_bab435dd:p.Ser3Ter"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_bab435dd:p.Ser3Ter"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_substitution_removes_stop_codon(self):
         hgvsc = "NM_999998.1:c.30G>T"
-        expected_hgvsp = "MD5_87c461c4:p.Ter10Argext*3"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Ter10Argext*3"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_insertion_no_frameshift(self):
         hgvsc = "NM_999999.1:c.6_7insGGG"
-        expected_hgvsp = "MD5_87c461c4:p.Ala3_Lys4insGly"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Ala3_Lys4insGly"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_insertion_frameshift(self):
         hgvsc = "NM_999999.1:c.22_23insT"
-        expected_hgvsp = "MD5_87c461c4:p.Ala8Valfs*?"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Ala8Valfs*?"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_insertion_adds_stop(self):
         hgvsc = "NM_999999.1:c.8_9insTT"
-        expected_hgvsp = "MD5_87c461c4:p.Lys4Ter"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys4Ter"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_deletion_no_frameshift(self):
         hgvsc = "NM_999999.1:c.10_12del"
-        expected_hgvsp = "MD5_87c461c4:p.Lys4del"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys4del"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_deletion2_no_frameshift(self):
         hgvsc = "NM_999999.1:c.4_15del"
-        expected_hgvsp = "MD5_87c461c4:p.Lys2_Ala5del"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys2_Ala5del"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_deletion_frameshift_nostop(self):
         hgvsc = "NM_999999.1:c.11_12del"
-        expected_hgvsp = "MD5_87c461c4:p.Lys4Serfs*?"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys4Serfs*?"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_deletion_frameshift_adds_stop(self):
         hgvsc = "NM_999997.1:c.7del"
-        expected_hgvsp = "MD5_c124d888:p.Ala3Argfs*6"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_c124d888:p.Ala3Argfs*6"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_indel_no_frameshift(self):
         hgvsc = "NM_999999.1:c.11_12delinsTCCCA"
-        expected_hgvsp = "MD5_87c461c4:p.Lys4delinsIlePro"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys4delinsIlePro"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_indel2_no_frameshift(self):
         hgvsc = "NM_999999.1:c.11_18delinsTCCCA"
-        expected_hgvsp = "MD5_87c461c4:p.Lys4_Phe6delinsIlePro"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys4_Phe6delinsIlePro"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_indel_frameshift_nostop(self):
         hgvsc = "NM_999999.1:c.8delinsGG"
-        expected_hgvsp = "MD5_87c461c4:p.Ala3Glyfs*?"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Ala3Glyfs*?"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_dup_1AA_no_frameshift_2(self):
         hgvsc = "NM_999999.1:c.10_12dup"
-        expected_hgvsp = "MD5_87c461c4:p.Lys4dup"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Lys4dup"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_dup_1AA_no_frameshift(self):
         hgvsc = "NM_999999.1:c.16_18dup"
-        expected_hgvsp = "MD5_87c461c4:p.Phe6dup"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Phe6dup"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_dup_2AA_no_frameshift(self):
         hgvsc = "NM_999999.1:c.16_21dup"
-        expected_hgvsp = "MD5_87c461c4:p.Phe6_Arg7dup"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Phe6_Arg7dup"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_dup_3AA_no_frameshift(self):
         hgvsc = "NM_999999.1:c.16_24dup"
-        expected_hgvsp = "MD5_87c461c4:p.Phe6_Ala8dup"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Phe6_Ala8dup"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_dup_frameshift(self):
         hgvsc = "NM_999999.1:c.12_13dup"
-        expected_hgvsp = "MD5_87c461c4:p.Ala5Glufs*?"
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.Ala5Glufs*?"
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_intron(self):
         hgvsc = "NM_999999.1:c.12+1G>A"
-        expected_hgvsp = "MD5_87c461c4:p.="
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.="
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_five_prime_utr(self):
         hgvsc = "NM_999999.1:c.-2A>G"
-        expected_hgvsp = "MD5_87c461c4:p.="
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.="
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     def test_three_prime_utr(self):
         hgvsc = "NM_999999.1:c.*3G>A"
-        expected_hgvsp = "MD5_87c461c4:p.="
-        self._run_conversion(hgvsc, expected_hgvsp)
+        hgvsp_expected = "MD5_87c461c4:p.="
+        self._run_conversion(hgvsc, hgvsp_expected)
 
     # The following are unsupported
     #
     # def test_repeats(self):
     #     hgvsc = "NM_999999.1:c.12_13[3]"
-    #     expected_hgvsp = ""
-    #     self._run_conversion(hgvsc, expected_hgvsp)
+    #     hgvsp_expected = ""
+    #     self._run_conversion(hgvsc, hgvsp_expected)
     #
     # def test_variable_repeats(self):
     #     pass
     #
     # def test_extension_new_translation_initiation_site(self):
     #     hgvsc = "NM_999999.1:c.1A>G"
-    #     expected_hgvsp = ""
-    #     self._run_conversion(hgvsc, expected_hgvsp)
+    #     hgvsp_expected = ""
+    #     self._run_conversion(hgvsc, hgvsp_expected)
     #
     # def test_indeterminate_entire_exon_del(self):
     #     pass
@@ -160,13 +162,14 @@ class TestHgvsCToP(unittest.TestCase):
     # def test_two_changes_unknown_allele(self):
     #     pass
 
-    def _run_conversion(self, hgvsc, expected_hgvsp):
+    def _run_conversion(self, hgvsc, hgvsp_expected):
         """Helper method to actually run the test
         :param hgvsc tag
         """
-        actual_hgvsp = TestHgvsCToP._translator.hgvsc_to_hgvsp(hgvsc)
-        msg = "hgvsp expected: {} actual: {}".format(expected_hgvsp, actual_hgvsp)
-        self.assertEqual(expected_hgvsp, actual_hgvsp, msg)
+        var_c = TestHgvsCToP._parser.parse_hgvs_variant(hgvsc)
+        hgvsp_actual = str(TestHgvsCToP._translator.hgvsc_to_hgvsp(var_c))
+        msg = "hgvsp expected: {} actual: {}".format(hgvsp_expected, hgvsp_actual)
+        self.assertEqual(hgvsp_expected, hgvsp_actual, msg)
 
     # TODO - review other classes of hgvs tags (e.g. utr, intronic) - more use cases?
     # 5'utr
