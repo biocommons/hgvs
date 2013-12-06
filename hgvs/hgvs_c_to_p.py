7ecd3f0b19b265d455f30b13079bd0702b623963
Rudy Rico <rudy.rico@invitae.com>
2013-12-06 10:31:00 -0800

    revamped hgvs_c_to_p so its interface matches hgvsmapper; should make incorporation a simple matter of copying the hgvsc_to_hgvsp method in.    Updated tests accordingly.    Moved tests to top-level.

--- hgvs/hgvs_c_to_p.py
+++ hgvs/hgvs_c_to_p.py
@@ -12,24 +12,15 @@ import hgvs.utils.altseq_to_hgvsp as altseq_to_hgvsp
 import hgvs.utils.altseqbuilder as altseqbuilder
 
 
-class RefTranscriptData(recordtype.recordtype('RefTranscriptData',
-                                              ['transcript_sequence', 'aa_sequence',
-                                               'cds_start', 'cds_stop', 'protein_accession'])):
-    pass
-
-
 class HgvsCToP(object):
 
-    def __init__(self, datasource):
-        """Constructor
+    def __init__(self,db=None,cache_transcripts=False):
+        self.db = db
+        self.cache_transcripts = cache_transcripts
+        self.hgvs_parser = hgvs.parser.Parser()
+        self.tm_cache = {}
 
-        :param datasource
-        :type base_input_source
-        """
-        self.datasource = datasource
-        self.parser = hgvs.parser.Parser()
-
-    def convert(self, hgvsc):
+    def hgvsc_to_hgvsp(self, hgvsc):
         """Convert hgvsc tag to hgvsp tag
 
         :param hgvsc: hgvsc tag
@@ -38,17 +29,44 @@ class HgvsCToP(object):
         :type SequenceVariant
         """
 
-        # translate hgvsc
-        var = self.parser.parse_hgvs_variant(hgvsc)
+        class RefTranscriptData(recordtype.recordtype('RefTranscriptData',
+                                                      ['transcript_sequence', 'aa_sequence',
+                                                       'cds_start', 'cds_stop', 'protein_accession'])):
 
-        # get transcript from datasource & convert to AA
-        reference_data = self._setup_transcript_data(var.seqref)
+            @classmethod
+            def setup_transcript_data(cls, ac, db, ref='GRCh37.p10'):
+                """helper for generating RefTranscriptData from for hgvsc_to_hgvsp"""
+                ts_exons = db.get_tx_exons(ac)
+                ts_info = db.get_tx_info(ac)
 
-        # incorporate DNA change into sequence(s) and translate
+                # use 1-based hgvs coords
+                cds_start = ts_info['cds_start_i'] + 1
+                cds_stop = ts_info['cds_stop_i']
+
+                # concatenate sequences by exon
+                ord_seq = {x['ord']: x['t_seq_a'] for x in ts_exons}
+                ordered_keys = ord_seq.keys()
+                ordered_keys.sort()
+                transcript_sequence = ''.join(ord_seq[x] for x in ordered_keys)
+
+                transcript_dna_cds = Seq(transcript_sequence[cds_start - 1:cds_stop])
+                protein_seq = str(transcript_dna_cds.translate())
+                protein_acc = hgvs.stopgap.pseq_to_ac(protein_seq)
+
+                transcript_data = RefTranscriptData(transcript_sequence, protein_seq, cds_start,
+                                                    cds_stop, protein_acc)
+
+                return transcript_data
+
+
+        var = self.hgvs_parser.parse_hgvs_variant(hgvsc)
+        reference_data = RefTranscriptData.setup_transcript_data(var.seqref, self.db)
         builder = altseqbuilder.AltSeqBuilder(var, reference_data)
+
+        # TODO - handle case where you get 2+ alt sequences back; currently get list of 1 element
+        # loop structure implemented to handle this, but doesn't really do anything currently.
         alt_data = builder.build_altseq()
 
-        # perform comparison to get hgvs tag
         hgvsps = []
         for alt in alt_data:
             builder = altseq_to_hgvsp.AltSeqToHgvsp(reference_data.aa_sequence,
@@ -59,43 +77,7 @@ class HgvsCToP(object):
             hgvsp = builder.build_hgvsp()
             hgvsps.append(hgvsp)
 
-        hgvsp = hgvsps[0]    # TODO - handle case of more than 1 variant
+        hgvsp = hgvsps[0]
 
-        return hgvsp
+        return str(hgvsp)
 
-    #
-    # internal methods
-    #
-
-    def _setup_transcript_data(self, ac):
-        """wrapper to convert transcript data from external source into common form
-
-        :param ac accession #
-        :type str
-        :return transcript info
-        :type recordtype
-        """
-        ts_exons = self.datasource.fetch_transcript_exons(ac, 'GRCh37.p10')
-        ts_info = self.datasource.fetch_transcript_info(ac)
-
-        # use 1-based hgvs coords
-        cds_start = ts_info['cds_start_i'] + 1
-        cds_stop = ts_info['cds_stop_i']
-
-        # concatenate sequences by exon
-        ord_seq = {x['ord']: x['t_seq_a'] for x in ts_exons}
-        ordered_keys = ord_seq.keys()
-        ordered_keys.sort()
-        transcript_sequence = ''.join(ord_seq[x] for x in ordered_keys)
-
-        transcript_dna_cds = Seq(transcript_sequence[cds_start - 1:cds_stop])
-        protein_seq = str(transcript_dna_cds.translate())
-        protein_acc = hgvs.stopgap.pseq_to_ac(protein_seq)
-
-        transcript_data = RefTranscriptData(transcript_sequence,
-                                            protein_seq,
-                                            cds_start,
-                                            cds_stop,
-                                            protein_acc)
-
-        return transcript_data
