91c7540bdc0c3a9b1cb1e0b6627055b849fba05d
Reece Hart <reecehart@gmail.com>
2013-12-16 00:56:26 -0800

    added accession lookup for all of RefSeq protein

--- hgvs/stopgap.py
+++ hgvs/stopgap.py
@@ -4,8 +4,19 @@ stopgap -- components that need to be fleshed out later
 importing stopgap should tell you that you're using incomplete and/or unstable code
 """
 
-import hashlib
+import gzip
+import json
+from pkg_resources import resource_filename
 
-def pseq_to_ac(s):
-    "placeholder function that will return an accession for a given protein sequence"
-    return 'MD5_' + hashlib.md5(s).hexdigest()[:8]
+from Bio.SeqUtils.CheckSum import seguid
+
+seguid_acs = json.loads(gzip.open(resource_filename(__name__,'/data/seguid-acs.json.gz')).read())
+
+def pseq_to_ac(seq):
+    "placeholder function that returns an accession for a given protein sequence"
+    seq = seq.rstrip('*')
+    hash = seguid(seq)
+    try:
+        return seguid_acs[hash][0]
+    except KeyError:
+        return 'SEGUID_' + hash
