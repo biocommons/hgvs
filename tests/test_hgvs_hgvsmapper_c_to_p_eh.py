040eca68d59d5eb9f9e6d4eb59ae1223afb215d9
Rudy Rico <rudy.rico@invitae.com>
2013-12-19 18:32:29 -0800

    Convert test input and consumer to use 4-column format

--- tests/test_hgvs_hgvsmapper_c_to_p_eh.py
+++ tests/test_hgvs_hgvsmapper_c_to_p_eh.py
@@ -10,11 +10,8 @@ import hgvs.hgvsmapper as hgvsmapper
 import hgvs.parser
 import uta.db.transcriptdb
 
-#INFILE = 'eh_tests.tsv'
-INFILE = 'eh_c_to_p.tsv'
-#INFILE = 'eh_c_to_p_2.tsv'
+INFILE = 'eh_tests.tsv'
 OUTFILE = 'eh_c_to_p.out'
-#OUTFILE = 'eh_c_to_p_2.out'
 
 class TestHgvsCToPReal(unittest.TestCase):
 
@@ -26,7 +23,13 @@ class TestHgvsCToPReal(unittest.TestCase):
     #     """For purposes of tesing a single result"""
     #     hgvsc = 'NM_000257.2:c.3300_3311delCGGCAGCCAGCT'
     #     hgvsp_expected = 'NP_000248.2:p.Gly1101_Leu1104del'
-    #     self._run_conversion(hgvsc, hgvsp_expected)
+    #
+    #     var_c = TestHgvsCToPReal._parser.parse_hgvs_variant(hgvsc)
+    #     var_p = TestHgvsCToPReal._mapper.hgvsc_to_hgvsp(var_c)
+    #     print "acc {}".format(var_p.ac)
+    #     hgvsp_actual = str(var_p)
+    #     msg = "hgvsp expected: {} actual: {}".format(hgvsp_expected, hgvsp_actual)
+    #     self.assertEqual(hgvsp_expected, hgvsp_actual, msg)
 
 
     def test_eh(self):
@@ -34,64 +37,42 @@ class TestHgvsCToPReal(unittest.TestCase):
         fn = os.path.join(os.path.dirname(__file__), 'data', INFILE)
         fo = os.path.join(os.path.dirname(__file__), 'data', OUTFILE)
         ff = open(fo, 'w')
-        ff.write("NM coordinates\tProtein coordinates\tConverter coordinates\tError\n")
+        ff.write("id\tHGVSg\tHGVSc\tHGVSp\tConverterResult\tError\n")
         with open(fn, 'r') as f:
             csvreader = csv.DictReader(f, delimiter='\t')
-            testdata = [(row['NM coordinates'], row['Protein coordinates']) for row in csvreader]
+            testdata = ((row['id'], row['HGVSg'], row['HGVSc'], row['HGVSp']) for row in csvreader)
 
-        failed_tests = []
-        for x in testdata:
-            print x
-            (hgvsc, hgvsp_expected) = x
-            if not hgvsc.startswith("#"):
-                try:
-                    hgvsp_actual = self._run_conversion_batch(hgvsc)
-                    msg = "hgvsp expected: {} actual: {}".format(hgvsp_expected, hgvsp_actual)
-                    if hgvsp_expected != hgvsp_actual:
-                        if hgvsp_expected.endswith("*") \
-                            and hgvsp_actual.endswith("Ter") and hgvsp_expected[:-1] == hgvsp_actual[:-3]:
-                            pass
-                        else:
-                            out = (hgvsc, hgvsp_expected, hgvsp_actual)
-                            failed_tests.append(out)
-                            message = "MISMATCH"
-                            ff.write("{}\t{}\t{}\t{}\n".format(hgvsc, hgvsp_expected, hgvsp_actual, message))
-                except Exception as e:
-                    hgvsp_actual = "NO_OUTPUT_EXCEPTION"
-                    out = (hgvsc, hgvsp_expected, hgvsp_actual)
-                    failed_tests.append(out)
-                    ff.write("{}\t{}\t{}\t{}\n".format(hgvsc, hgvsp_expected, hgvsp_actual, e.message))
+            failed_tests = []
+            for x in testdata:
+                (id, hgvsg, hgvsc, hgvsp_expected) = x
+                if not id.startswith("#"):
+                    try:
+                        var_c = TestHgvsCToPReal._parser.parse_hgvs_variant(hgvsc)
+                        var_p = TestHgvsCToPReal._mapper.hgvsc_to_hgvsp(var_c)
+                        hgvsp_actual = str(var_p)
+                        msg = "hgvsp expected: {} actual: {}".format(hgvsp_expected, hgvsp_actual)
+                        if hgvsp_expected != hgvsp_actual:
+                            if hgvsp_expected.endswith("*") \
+                                and hgvsp_actual.endswith("Ter") and hgvsp_expected[:-1] == hgvsp_actual[:-3]:
+                                pass    # skip * vs Ter for now
+                            else:
+                                out = (hgvsc, hgvsp_expected, hgvsp_actual)
+                                failed_tests.append(out)
+                                message = "MISMATCH"
+                                ff.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(id, hgvsg, hgvsc, hgvsp_expected,
+                                                                           hgvsp_actual, message))
+                    except Exception as e:
+                        hgvsp_actual = "NO_OUTPUT_EXCEPTION"
+                        out = (hgvsc, hgvsp_expected, hgvsp_actual)
+                        failed_tests.append(out)
+                        ff.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(id, hgvsg, hgvsc, hgvsp_expected,
+                                                                   hgvsp_actual, e.message))
 
         ff.close()
         print len(failed_tests)
         print failed_tests
         self.assertTrue(len(failed_tests) == 0)
 
-    #
-    # internal methods
-    #
-
-    def _run_conversion(self, hgvsc, hgvsp_expected):
-        """Helper method to actually run the test
-        :param hgvsc tag
-        """
-        var_c = TestHgvsCToPReal._parser.parse_hgvs_variant(hgvsc)
-        var_p = TestHgvsCToPReal._mapper.hgvsc_to_hgvsp(var_c)
-        print "acc {}".format(var_p.ac)
-        hgvsp_actual = str(var_p)
-        msg = "hgvsp expected: {} actual: {}".format(hgvsp_expected, hgvsp_actual)
-        self.assertEqual(hgvsp_expected, hgvsp_actual, msg)
-
-
-
-
-    def _run_conversion_batch(self, hgvsc):
-        """Helper method to actually run the test
-        :param hgvsc tag
-        """
-        var_c = TestHgvsCToPReal._parser.parse_hgvs_variant(hgvsc)
-        var_p = TestHgvsCToPReal._mapper.hgvsc_to_hgvsp(var_c)
-        return str(var_p)
 
 
 
