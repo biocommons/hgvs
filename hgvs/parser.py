5a4a9b144be28eea72715c4a5c17091267329244
Reece Hart <reecehart@gmail.com>
2013-12-30 13:10:47 -0800

    added Apache license and code boilerplate to all source files and scripts

--- hgvs/parser.py
+++ hgvs/parser.py
@@ -37,3 +37,19 @@ class Parser(object):
         rule_fxn = lambda s: self._grammar(s).__getattr__(rule_name)()
         rule_fxn.func_doc = "parse string s using `%s' rule" % rule_name
         return rule_fxn
+
+## <LICENSE>
+## Copyright 2014 HGVS Contributors (https://bitbucket.org/invitae/hgvs)
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##     http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## </LICENSE>
