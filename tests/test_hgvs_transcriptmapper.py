38b4145c440261a5aa100ac9943ba80672c454e1
Reece Hart <reecehart@gmail.com>
2013-12-30 13:10:47 -0800

    added Apache license and code boilerplate to all source files and scripts

--- tests/test_hgvs_transcriptmapper.py
+++ tests/test_hgvs_transcriptmapper.py
@@ -318,3 +318,19 @@ if __name__ == '__main__':
 #     ac = 'NM_145171.3'
 #     tm = TranscriptMapper(self.bdi,ac,self.ref)
 #     pass
+
+## <LICENSE>
+## Copyright 2014 HGVS Contributors (https://bitbucket.org/invitae/hgvs)
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##     http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## </LICENSE>
