767e9861560e118423cc04fb7a440916deccd2ad
Reece Hart <reecehart@gmail.com>
2013-12-31 10:25:13 -0800

    updated doc static images

--- doc/conf.py
+++ doc/conf.py
@@ -111,7 +111,7 @@ html_title = '{project} {release}'.format(project = project, release = release)
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'static/logo.png'
+html_logo = 'static/hgvs-logo.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
