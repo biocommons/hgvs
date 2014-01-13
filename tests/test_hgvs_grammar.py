6d89b2194a487c1eacfb53223ba49b48c281a316
Reece Hart <reecehart@gmail.com>
2014-01-13 12:36:26 -0800

    CORE-99: fix aa13t parsing, take 2; tests pass (including G* test)

--- tests/test_hgvs_grammar.py
+++ tests/test_hgvs_grammar.py
@@ -28,19 +28,40 @@ class Test_Parser(unittest.TestCase):
         self.assertEqual( self.grammar('LRG_1234').accn(), 'LRG_1234' )
         self.assertEqual( self.grammar('LRG_1234t1').accn(), 'LRG_1234t1' )
 
+    def test_parser_aa(self):
+        # terms
+        self.assertEqual( self.p.parse_term1('*'), '*' )
+        self.assertEqual( self.p.parse_term3('Ter'), 'Ter' )
+        self.assertEqual( self.p.parse_term13('*'), '*' )
+        self.assertEqual( self.p.parse_term13('Ter'), 'Ter' )
+
+        # AAs
+        self.assertEqual( self.p.parse_aa1('A'), 'A' )
+        self.assertEqual( self.p.parse_aa3('Ala'), 'Ala' )
+        self.assertEqual( self.p.parse_aa13('A'), 'A' )
+        self.assertEqual( self.p.parse_aa13('Ala'), 'Ala' )
+
+        # mixto
+        self.assertEqual( self.p.parse_aat1('*'), '*' )
+        self.assertEqual( self.p.parse_aat1('A'), 'A' )
+        self.assertEqual( self.p.parse_aat3('Ter'), 'Ter' )
+        self.assertEqual( self.p.parse_aat3('Ala'), 'Ala' )
+        self.assertEqual( self.p.parse_aat13('*'), '*' )
+        self.assertEqual( self.p.parse_aat13('A'), 'A' )
+        self.assertEqual( self.p.parse_aat13('Ter'), 'Ter' )
+        self.assertEqual( self.p.parse_aat13('Ala'), 'Ala' )
+
     def test_parser_aa_seq(self):
-        self.assertEqual( self.p.parse_aa3t_seq('Ala'), 'Ala' )
-        self.assertEqual( self.p.parse_aa1t_seq('A'), 'A' )
-        self.assertEqual( self.p.parse_aa13t_seq('Ala'), 'Ala' )
-        self.assertEqual( self.p.parse_aa13t_seq('A'), 'A' )
-        self.assertEqual( self.p.parse_aa3t_seq('AlaAla'), 'AlaAla' )
-        self.assertEqual( self.p.parse_aa1t_seq('AA'), 'AA' )
-        self.assertEqual( self.p.parse_aa13t_seq('AlaAla'), 'AlaAla' )
-        self.assertEqual( self.p.parse_aa13t_seq('AA'), 'AA' )
-        self.assertEqual( self.p.parse_aa3t_seq('AlaAlaTer'), 'AlaAlaTer' )
-        self.assertEqual( self.p.parse_aa1t_seq('AA*'), 'AA*' )
-        self.assertEqual( self.p.parse_aa13t_seq('AlaAlaTer'), 'AlaAlaTer' )
-        self.assertEqual( self.p.parse_aa13t_seq('AA*'), 'AA*' )
+        # mixto
+        self.assertEqual( self.p.parse_aat1_seq('*'), '*' )
+        self.assertEqual( self.p.parse_aat1_seq('A'), 'A' )
+        self.assertEqual( self.p.parse_aat3_seq('Ter'), 'Ter' )
+        self.assertEqual( self.p.parse_aat3_seq('Ala'), 'Ala' )
+        self.assertEqual( self.p.parse_aat13_seq('*'), '*' )
+        self.assertEqual( self.p.parse_aat13_seq('A'), 'A' )
+        self.assertEqual( self.p.parse_aat13_seq('Ter'), 'Ter' )
+        self.assertEqual( self.p.parse_aat13_seq('Ala'), 'Ala' )
+
 
 
 if __name__ == '__main__':
