60f76aca6daf4b70bfa76096b2cc852ec0cb5899
Rudy Rico <rudy.rico@invitae.com>
2014-01-24 12:56:33 -0800

    CORE-114 - add test that checks that all rules have been tested - and add tests for rules that were missed!

--- tests/test_hgvs_grammar_full.py
+++ tests/test_hgvs_grammar_full.py
@@ -26,6 +26,8 @@ import csv
 import pprint
 import unittest
 
+import pkg_resources
+
 import hgvs.parser
 
 class TestGrammarFull(unittest.TestCase):
@@ -33,10 +35,31 @@ class TestGrammarFull(unittest.TestCase):
     def setUp(self):
         self.p = hgvs.parser.Parser()
         self.grammar = self.p._grammar
+        self._test_fn = os.path.join(os.path.dirname(__file__), 'data', 'grammar_test.tsv')
+
+    def test_parser_test_completeness(self):
+
+        rules_tested = set()
+
+        with open(self._test_fn, 'r') as f:
+            reader = csv.DictReader(f, delimiter='\t')
+            for row in reader:
+                rules_tested.add(row['Func'])
+
+        rules_all= set()
+        grammar_fn = pkg_resources.resource_filename(__name__, '../hgvs/data/hgvs.pymeta')
+        for line in open(grammar_fn, 'r'):
+            if len(line) > 0 and line[0] != '#' and line[0].isalpha():
+                line = line.strip()
+                rules_all.add(line.split()[0])
+
+        rules_untested = rules_all - rules_tested
+
+        msg = "untested rules: {}".format(rules_untested)
+        self.assertTrue(len(rules_untested) == 0, msg)
 
     def test_parser_grammar(self):
-        fn = os.path.join(os.path.dirname(__file__), 'data', 'grammar_test.tsv')
-        with open(fn, 'r') as f:
+        with open(self._test_fn, 'r') as f:
             reader = csv.DictReader(f, delimiter='\t')
 
             fail_cases = []
