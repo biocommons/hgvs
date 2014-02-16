601d0831110d6a90b8a5fce1ec6f76d75b286989
Reece Hart <reecehart@gmail.com>
2014-02-16 10:59:25 -0800

    Makefile, setup,py, setup.cfg sync with sibling projects

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     )
 
 ## <LICENSE>
-## Copyright 2014 UTA Contributors (https://bitbucket.org/invitae/uta)
+## Copyright 2014 HGVS Contributors (https://bitbucket.org/invitae/hgvs)
 ## 
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
