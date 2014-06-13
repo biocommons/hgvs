99e1f000672bbf8a4c09fd6ed5c0d8a06f668acc
Reece Hart <reecehart@gmail.com>
2014-06-13 13:14:59 -0700

    #162: provide simplified mapping interface

--- tests/test_hgvs_dataproviders_uta.py
+++ tests/test_hgvs_dataproviders_uta.py
@@ -6,61 +6,6 @@ import hgvs.dataproviders.uta
 
 class UTA_Base(object):
 
-    def test_get_tx_exons(self):    
-        tx_exons = self.hdp.get_tx_exons('NM_000551.3','NC_000003.11', 'splign')
-        self.assertEqual( 3, len(tx_exons) )
-
-    def test_get_tx_info(self):
-        tx_info = self.hdp.get_tx_info('NM_000051.3', 'AC_000143.1', 'splign')
-        self.assertEqual( 385, tx_info['cds_start_i'])
-        self.assertEqual( 9556, tx_info['cds_end_i'])
-        self.assertEqual( 'AC_000143.1', tx_info['alt_ac'])
-
-    def test_get_tx_mapping_options(self):
-        tx_mapping_options = self.hdp.get_tx_mapping_options('NM_000551.3')
-        self.assertEqual( 4, len(tx_mapping_options) )
-        self.assertIn( ['NM_000551.3', 'NC_000003.11', 'splign'], tx_mapping_options )
-        self.assertIn( ['NM_000551.3', 'NC_000003.11', 'blat']  , tx_mapping_options )
-
-    def test_get_tx_seq(self):
-        tsi = self.hdp.get_tx_seq('NM_000551.3')
-        seq = tsi
-        self.assertEqual( 4560, len(seq) )
-        self.assertTrue( seq.startswith('cctcgcctccgttacaacgg'.upper()) )
-        self.assertTrue( seq.endswith('aaaggaaatag'.upper()) )
-
-    def test_get_tx_for_gene(self):
-        tig = self.hdp.get_tx_for_gene('VHL')
-        self.assertEqual( 11, len(tig) )
-
-    def test_get_tx_exons_invalid_tx_ac(self):
-        tx_exons = self.hdp.get_tx_exons('NM_999999.9','NC_000003.11', 'splign')
-        self.assertIsNone( tx_exons )
-
-    def test_get_tx_exons_invalid_alt_ac(self):
-        tx_exons = self.hdp.get_tx_exons('NM_000551.3','NC_000999.9', 'splign')
-        self.assertIsNone( tx_exons )
-
-    def test_get_tx_exons_invalid_alt_aln_method(self):
-        tx_exons = self.hdp.get_tx_exons('NM_000551.3','NC_000999.9', 'best')
-        self.assertIsNone( tx_exons )
-
-    def test_get_tx_info_invalid_tx_ac(self):
-        tx_info = self.hdp.get_tx_info('NM_999999.9', 'AC_000143.1', 'splign')
-        self.assertIsNone( tx_info )
-
-    def test_get_tx_mapping_options_invalid(self):
-        tx_info_options = self.hdp.get_tx_mapping_options('NM_999999.9')
-        self.assertIsNone( tx_info_options )
-
-    def test_get_tx_seq_invalid_ac(self):
-        tsi = self.hdp.get_tx_seq('NM_999999.9')
-        self.assertIsNone( tsi )
-
-    def test_get_tx_for_gene_invalid_gene(self):
-        tig = self.hdp.get_tx_for_gene('GENE')
-        self.assertEqual( 0, len(tig) )
-
     def test_get_acs_for_protein_seq(self):
         exp = ['NP_001005405.1','MD5_8fc09b1d9a38a8c55176a0fa922df227']
         s = """
@@ -86,6 +31,63 @@ class UTA_Base(object):
         self.assertEqual( 'VHL', gene_info['hgnc'])
         self.assertEqual( '3p25.3', gene_info['maploc'])
         self.assertEqual( 6, len(gene_info))
+
+    def test_get_tx_exons(self):    
+        tx_exons = self.hdp.get_tx_exons('NM_000551.3','NC_000003.11', 'splign')
+        self.assertEqual( 3, len(tx_exons) )
+
+    def test_get_tx_exons_invalid_tx_ac(self):
+        tx_exons = self.hdp.get_tx_exons('NM_999999.9','NC_000003.11', 'splign')
+        self.assertIsNone( tx_exons )
+
+    def test_get_tx_exons_invalid_alt_ac(self):
+        tx_exons = self.hdp.get_tx_exons('NM_000551.3','NC_000999.9', 'splign')
+        self.assertIsNone( tx_exons )
+
+    def test_get_tx_exons_invalid_alt_aln_method(self):
+        tx_exons = self.hdp.get_tx_exons('NM_000551.3','NC_000999.9', 'best')
+        self.assertIsNone( tx_exons )
+
+    def test_get_tx_for_gene(self):
+        tig = self.hdp.get_tx_for_gene('VHL')
+        self.assertEqual( 11, len(tig) )
+
+    def test_get_tx_for_gene_invalid_gene(self):
+        tig = self.hdp.get_tx_for_gene('GENE')
+        self.assertEqual( 0, len(tig) )
+
+
+    def test_get_tx_info(self):
+        tx_info = self.hdp.get_tx_info('NM_000051.3', 'AC_000143.1', 'splign')
+        self.assertEqual( 385, tx_info['cds_start_i'])
+        self.assertEqual( 9556, tx_info['cds_end_i'])
+        self.assertEqual( 'AC_000143.1', tx_info['alt_ac'])
+
+    def test_get_tx_info_invalid_tx_ac(self):
+        tx_info = self.hdp.get_tx_info('NM_999999.9', 'AC_000143.1', 'splign')
+        self.assertIsNone( tx_info )
+
+    def test_get_tx_mapping_options(self):
+        tx_mapping_options = self.hdp.get_tx_mapping_options('NM_000551.3')
+        self.assertEqual( 4, len(tx_mapping_options) )
+        self.assertIn( ['NM_000551.3', 'NC_000003.11', 'splign'], tx_mapping_options )
+        self.assertIn( ['NM_000551.3', 'NC_000003.11', 'blat']  , tx_mapping_options )
+
+    def test_get_tx_mapping_options_invalid(self):
+        tx_info_options = self.hdp.get_tx_mapping_options('NM_999999.9')
+        self.assertEqual( tx_info_options, [] )
+
+    def test_get_tx_seq(self):
+        tsi = self.hdp.get_tx_seq('NM_000551.3')
+        seq = tsi
+        self.assertEqual( 4560, len(seq) )
+        self.assertTrue( seq.startswith('cctcgcctccgttacaacgg'.upper()) )
+        self.assertTrue( seq.endswith('aaaggaaatag'.upper()) )
+
+    def test_get_tx_seq_invalid_ac(self):
+        tsi = self.hdp.get_tx_seq('NM_999999.9')
+        self.assertIsNone( tsi )
+
 
 
 class Test_hgvs_dataproviders_uta_UTA_default(unittest.TestCase, UTA_Base):
--- tests/test_hgvs_variantmapper.py
+++ tests/test_hgvs_variantmapper.py
@@ -1,25 +1,28 @@
 import unittest
 
-import hgvs.dataproviders.uta
+from nose.plugins.attrib import attr
 
+import hgvs.dataproviders.uta
 import hgvs.parser
 import hgvs.variantmapper
 
+
+@attr(tags=["quick"])
 class Test_VariantMapper(unittest.TestCase):
     def setUp(self):
         self.hdp = hgvs.dataproviders.uta.connect()
-        self.hm = hgvs.variantmapper.VariantMapper(self.hdp, cache_transcripts=True)
+        self.vm = hgvs.variantmapper.VariantMapper(self.hdp, cache_transcripts=True)
         self.hp = hgvs.parser.Parser()
 
-    def test_1(self):
+    def test_VariantMapper_quick(self):
         # From garcia.tsv:
         hgvs_g = 'NC_000007.13:g.36561662C>T'
         hgvs_c = 'NM_001637.3:c.1582G>A'
         hgvs_p = 'NP_001628.1:p.(Gly528Arg)'  # from Mutalyzer
 
         var_g = self.hp.parse_hgvs_variant(hgvs_g)
-        var_c = self.hm.g_to_c( var_g, 'NM_001637.3' )
-        var_p = self.hm.c_to_p( var_c )
+        var_c = self.vm.g_to_c( var_g, 'NM_001637.3' )
+        var_p = self.vm.c_to_p( var_c )
 
         self.assertEqual( str(var_c) , hgvs_c )
         self.assertEqual( str(var_p) , hgvs_p )
@@ -32,13 +35,13 @@ class Test_VariantMapper(unittest.TestCase):
         var_g = self.hp.parse_hgvs_variant(hgvs_g)
         var_c = self.hp.parse_hgvs_variant(hgvs_c)
 
-        cases = {'gc': (self.hm.g_to_c, (var_c, 'NM_001637.3')),
-                 'gr': (self.hm.g_to_r, (var_c, 'NM_001637.3')),
-                 'rg': (self.hm.r_to_g, (var_c, 'NM_001637.3')),
-                 'cg': (self.hm.c_to_g, (var_g, 'NM_001637.3')),
-                 'cr': (self.hm.c_to_r, (var_g,)),
-                 'rc': (self.hm.r_to_c, (var_g,)),
-                 'cp': (self.hm.c_to_p, (var_g, None)),
+        cases = {'gc': (self.vm.g_to_c, (var_c, 'NM_001637.3')),
+                 'gr': (self.vm.g_to_r, (var_c, 'NM_001637.3')),
+                 'rg': (self.vm.r_to_g, (var_c, 'NM_001637.3')),
+                 'cg': (self.vm.c_to_g, (var_g, 'NM_001637.3')),
+                 'cr': (self.vm.c_to_r, (var_g,)),
+                 'rc': (self.vm.r_to_c, (var_g,)),
+                 'cp': (self.vm.c_to_p, (var_g, None)),
                  }
 
         failures = []
@@ -56,8 +59,39 @@ class Test_VariantMapper(unittest.TestCase):
         hgvs_g = 'NC_000007.13:g.36561662C>T'
         var_g = self.hp.parse_hgvs_variant(hgvs_g)
         with self.assertRaises(hgvs.exceptions.HGVSError):
-            var_p = self.hm.c_to_p(var_g, 'NM_999999.1')
+            var_p = self.vm.c_to_p(var_g, 'NM_999999.1')
 
+
+
+@attr(tags=["quick"])
+class Test_EasyVariantMapper(unittest.TestCase):
+    def setUp(self):
+        self.hdp = hgvs.dataproviders.uta.connect()
+        self.evm = hgvs.variantmapper.EasyVariantMapper(self.hdp,primary_assembly='GRCh37', alt_aln_method='splign')
+        self.hgvs = {
+            'g': 'NC_000007.13:g.36561662C>T',
+            'c': 'NM_001637.3:c.1582G>A',
+            'r': 'NM_001637.3:r.1983G>A',
+            'p': 'NP_001628.1:p.(Gly528Arg)'
+        }
+        hp = hgvs.parser.Parser()
+        self.var = { k:hp.parse_hgvs_variant(v) for k,v in self.hgvs.iteritems() }
+
+
+    def test_c_to_g(self):
+        self.assertEqual(self.hgvs['g'], str(self.evm.c_to_g(self.var['c'])))
+
+    def test_c_to_r(self):
+        self.assertEqual(self.hgvs['r'], str(self.evm.c_to_r(self.var['c'])))
+
+    def test_g_to_c(self):
+        self.assertEqual(self.hgvs['c'], str(self.evm.g_to_c(self.var['g'], self.var['c'].ac)))
+
+    def test_g_to_r(self):
+        self.assertEqual(self.hgvs['r'], str(self.evm.g_to_r(self.var['g'], self.var['r'].ac)))
+
+    def test_c_to_p(self):
+        self.assertEqual(self.hgvs['p'], str(self.evm.c_to_p(self.var['c'])))
 
 
 
