b40783a773546fef785d3cbbe9b96b91ba5ba46c
Reece Hart <reecehart@gmail.com>
2014-06-13 14:31:57 -0700

    remove unused args from VariantMapper.c_to_p

--- hgvs/variantmapper.py
+++ hgvs/variantmapper.py
@@ -186,13 +186,15 @@ class VariantMapper(object):
         return var_c
 
 
-    #TODO (API): alt_ac and alt_aln_method are not required; drop them
-    def c_to_p(self, var_c, alt_ac=None, alt_aln_method='transcript'):
+    # TODO: c_to_p needs refactoring
+    # TODO: data prep belongs in the data interface
+    def c_to_p(self, var_c, pro_ac=None):
         """
         Converts a c. SequenceVariant to a p. SequenceVariant on the specified protein accession
+        Author: Rudy Rico
 
         :param SequenceVariant var_c: hgvsc tag
-        :param str alt_ac: protein accession
+        :param str pro_ac: protein accession
         :rtype: hgvs.variant.SequenceVariant
 
         """
@@ -202,10 +204,9 @@ class VariantMapper(object):
                                                        'cds_start', 'cds_stop', 'protein_accession'])):
 
             @classmethod
-            def setup_transcript_data(cls, hdp, tx_ac, alt_ac, alt_aln_method):
+            def setup_transcript_data(cls, hdp, tx_ac, pro_ac):
                 """helper for generating RefTranscriptData from for c_to_p"""
                 tx_info = hdp.get_tx_identity_info(var_c.ac)
-                #tx_info = hdp.get_tx_info(tx_ac, tx_ac, alt_aln_method)  # want identity returned
                 tx_seq = hdp.get_tx_seq(tx_ac)
 
                 if tx_info is None or tx_seq is None:
@@ -223,19 +224,19 @@ class VariantMapper(object):
                 tx_seq_cds = Seq(tx_seq_to_translate)
                 protein_seq = str(tx_seq_cds.translate())
 
-                if alt_ac is None:
+                if pro_ac is None:
                     # get_acs... will always return at least the MD5_ accession
-                    alt_ac = hdp.get_acs_for_protein_seq(protein_seq)[0]
+                    pro_ac = hdp.get_acs_for_protein_seq(protein_seq)[0]
 
                 transcript_data = RefTranscriptData(tx_seq, protein_seq, cds_start,
-                                                    cds_stop, alt_ac)
+                                                    cds_stop, pro_ac)
 
                 return transcript_data
 
         if not (var_c.type == 'c'):
             raise hgvs.exceptions.InvalidHGVSVariantError('Expected a cDNA (c.); got ' + str(var_c))
 
-        reference_data = RefTranscriptData.setup_transcript_data(self.hdp, var_c.ac, alt_ac, alt_aln_method)
+        reference_data = RefTranscriptData.setup_transcript_data(self.hdp, var_c.ac, pro_ac)
         builder = altseqbuilder.AltSeqBuilder(var_c, reference_data)
 
         # TODO - handle case where you get 2+ alt sequences back; currently get list of 1 element
@@ -394,7 +395,7 @@ class EasyVariantMapper(VariantMapper):
         return super(EasyVariantMapper,self).r_to_c(var_r)
 
     def c_to_p(self, var_c): 
-        return super(EasyVariantMapper,self).c_to_p(var_c, alt_ac=None, alt_aln_method='transcript')
+        return super(EasyVariantMapper,self).c_to_p(var_c)
 
     def relevant_transcripts(self,var_g):
         """return list of transcripts accessions (strings) for given variant,
