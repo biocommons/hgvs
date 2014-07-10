793741a79c712954f4428518abfbab8b3e25afaf
Reece Hart <reecehart@gmail.com>
2014-07-09 22:55:52 -0700

    updated doc configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,44 +1,36 @@
-import sys, os
+############################################################################
+## Theme setup
+
+html_theme = 'invitae'
+#html_theme = 'f6'
+#html_theme = 'bootstrap'
+#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'pyramid'
+
+html_theme_path = ['../themes']
+if html_theme == 'sphinx_rtd_theme':
+    import sphinx_rtd_theme
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+elif html_theme == 'bootstrap':
+    import sphinx_bootstrap_theme
+    html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+print(html_theme, html_theme_path)
+
+
+############################################################################
+## Project config
 
 import hgvs
 version = hgvs.__version__
 release = str(hgvs.__version__)
 
 project = u'HGVS'
-copyright = u'2014, Invitae'
-
-source_suffix = '.rst'
-master_doc = 'index'
-exclude_patterns = ['build','static','templates','themes']
-pygments_style = 'sphinx'
-templates_path = ['templates']
-
-html_theme = 'invitae'
-html_theme_path = ['../themes']
-html_title = '{project} {release}'.format(project = project, release = release)
-html_logo = '../static/hgvs-logo.svg'
-html_favicon = '../static/favicon.ico'
-html_static_path = ['../static']
-
-autodoc_default_flags = ['members', 'undoc-members', 'show-inheritance'] #, 'inherited-members']
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.pngmath',
-    'sphinx.ext.todo',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.fulltoc',
-    ]
+authors = project + ' Contributors'
+copyright = u'2014, ' + authors
 
 extlinks={
-    'issue': ('https://bitbucket.org/invitae/hgvs/issue/%s' 'HGVS issue '),
-    }
-
-intersphinx_mapping = {
-    'http://docs.python.org/': None,
+    'issue': ('https://bitbucket.org/invitae/hgvs/issue/%s', 'HGVS issue '),
     }
 
 latex_documents = [
@@ -54,3 +46,31 @@ texinfo_documents = [
    u'HGVS Contributors', 'HGVS', 'One line description of project.',
    'Miscellaneous'),
 ]
+
+
+############################################################################
+## Boilerplate
+
+autodoc_default_flags = ['members', 'undoc-members', 'show-inheritance'] #, 'inherited-members']
+exclude_patterns = ['build','static','templates','themes']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.pngmath',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.fulltoc',
+    ]
+html_favicon = '../static/favicon.ico'
+html_logo = '../static/logo.svg'
+html_static_path = ['../static']
+html_title = '{project} {release}'.format(project = project, release = release)
+intersphinx_mapping = {
+    'http://docs.python.org/': None,
+    }
+master_doc = 'index'
+pygments_style = 'sphinx'
+source_suffix = '.rst'
+templates_path = ['templates']
