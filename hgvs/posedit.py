2b01f4411c92dcbd3eb3d13ea8cfc996deb7c6a3
Meng Wang <wangm0855@gmail.com>
2015-07-05 16:33:11 +0800

    Implement the parsing of Inversion Conversion, Mosaic and Chimeric variants

--- hgvs/posedit.py
+++ hgvs/posedit.py
@@ -9,7 +9,12 @@ class PosEdit( recordtype.recordtype( 'PosEdit', [('pos',None),('edit',None),('u
     """
     
     def __str__(self):
-        rv = str(self.edit) if self.pos is None else '{self.pos}{self.edit}'.format(self=self)
+        if self.edit and self.edit.type.startswith('mos_'):
+            rv = '[=/{self.edit}]'.format(self=self) if self.pos is None else '[=/{self.pos}{self.edit}]'.format(self=self)
+        elif self.edit and self.edit.type.startswith('chi_'):
+            rv = '[=//{self.edit}]'.format(self=self) if self.pos is None else '[=//{self.pos}{self.edit}]'.format(self=self)
+        else:
+            rv = str(self.edit) if self.pos is None else '{self.pos}{self.edit}'.format(self=self)
         if self.uncertain:
             if self.edit in ['0','']:
                 rv = rv + '?'
