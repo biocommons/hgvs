5c29a4acf09715bb73a17571bd0f22250fcd3253
Meng Wang <wangm0855@gmail.com>
2015-07-18 18:41:06 +0800

    Add class for CompoundVariant, MosaicVariant and ChimericVariant

--- hgvs/posedit.py
+++ hgvs/posedit.py
@@ -26,18 +26,46 @@ class PosEdit(recordtype.recordtype('PosEdit', [('pos', None), ('edit', None), (
         self.uncertain = True
         return self
 
-    ## <LICENSE>
-    ## Copyright 2014 HGVS Contributors (https://bitbucket.org/biocommons/hgvs)
-    ## 
-    ## Licensed under the Apache License, Version 2.0 (the "License");
-    ## you may not use this file except in compliance with the License.
-    ## You may obtain a copy of the License at
-    ## 
-    ##     http://www.apache.org/licenses/LICENSE-2.0
-    ## 
-    ## Unless required by applicable law or agreed to in writing, software
-    ## distributed under the License is distributed on an "AS IS" BASIS,
-    ## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    ## See the License for the specific language governing permissions and
-    ## limitations under the License.
-    ## </LICENSE>
+
+class PosEditSet(recordtype.recordtype('PosEditSet', ['posedits'])):
+    """
+    represents a list of PosEdit for ComplexVariant
+    """
+    
+    def __getitem__(self, i):
+        return self.posedits[i]
+
+    def __setitem__(self, i, value):
+        self.posedits[i] = value
+
+    def __len__(self):
+        return len(self.posedits)
+    
+    @property
+    def pos(self):
+        return [posedit.pos for posedit in self.posedits]
+    
+    @property
+    def edit(self):
+        return [posedit.edit for posedit in self.posedits]
+    
+    @property
+    def uncertain(self):
+        return [posedit.uncertain for posedit in self.posedits]
+
+
+## <LICENSE>
+## Copyright 2014 HGVS Contributors (https://bitbucket.org/biocommons/hgvs)
+## 
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+## 
+##     http://www.apache.org/licenses/LICENSE-2.0
+## 
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## </LICENSE>
