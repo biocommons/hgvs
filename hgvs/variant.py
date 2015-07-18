88681e131dd35678e801a82dde6729453bf02aa0
Meng <wangm0855@gmail.com>
2015-07-18 18:41:06 +0800

    Add class for CompoundVariant, MosaicVariant and ChimericVariant

--- hgvs/variant.py
+++ hgvs/variant.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from .decorators.deprecated import deprecated
 
+from hgvs.exceptions import HGVSInvalidVariantError
+import hgvs.posedit
 import recordtype
 
 
@@ -18,6 +20,153 @@ class SequenceVariant(recordtype.recordtype('SequenceVariant', ['ac', 'type', 'p
             return '{self.ac}:{self.type}.{self.posedit}'.format(self=self)
         else:
             return '{self.type}.{self.posedit}'.format(self=self)
+
+
+
+
+PHASE1 = 0
+PHASE2 = 1
+
+
+class ComplexVariant(recordtype.recordtype('ComplexVariant', ['variants', 'phases', 'uncertain'])):
+    """
+    The abstract class for CompoundVariant, MosiacVariant and ChimericVariant,
+    which are stored as a list of SequenceVariant and the phase info for each
+    variant in the list
+    """
+    
+    def __init__(self, variants, phases, uncertain=False):
+        super(ComplexVariant, self).__init__(variants, phases, uncertain)
+        self.separator = ''
+        if not self.all_same_type:
+            raise HGVSInvalidVariantError("ComplexVariant does not support variants "
+                                          "with different types.")
+        if self.phases and len(self.phases) != len(self.variants):
+            raise HGVSInvalidVariantError("The number of edits and the number of phase "
+                                          "info should be equal for ComplexVariant.")
+    
+    
+    
+    def __getitem__(self, i):
+        return self.variants[i]
+
+    def __setitem__(self, i, value):
+        self.variants[i] = value
+
+    def __len__(self):
+        return len(self.variants)
+    
+    def __str__(self):
+        var_type = self.variants[0].type
+        
+        if self.uncertain:
+            separator = '('+self.separator+')'
+        else:
+            separator = self.separator
+        
+        if self.phases:
+            vars1 = []
+            vars2 = []
+            for var, phase in zip(self.variants, self.phases):
+                if phase == PHASE1:
+                    vars1.append(var)
+                else:
+                    vars2.append(var)
+        
+        if self.all_same_ac:
+            var_ac = self.variants[0].ac
+            
+            if self.phases:
+                vars1_posedits = separator.join([ str(var.posedit) for var in vars1 ])
+                vars2_posedits = separator.join([ str(var.posedit) for var in vars2 ])
+                var_posedits   = '[' + vars1_posedits + '];[' + vars2_posedits + ']'
+            else:
+                var_posedits = separator.join([ str(var.posedit) for var in self.variants ])
+                if len(self.variants) > 1:
+                    var_posedits = '[' + var_posedits + ']'
+            
+            if var_ac is not None:
+                return '{ac}:{type}.{posedits}'.format(ac=var_ac, type=var_type, posedits=var_posedits)
+            else:
+                return '{type}.{posedits}'.format(type=var_type, posedits=var_posedits)
+        else:
+            if self.phases:
+                vars1_var = separator.join(map(str, vars1))
+                vars2_var = separator.join(map(str, vars2))
+                vars      = '[' + vars1_var + '];[' + vars2_var + ']'
+            else:
+                vars = separator.join(map(str, self.variants))
+                vars = '[' + vars + ']'
+            
+            return vars
+    
+    
+    def _set_uncertain(self):
+        self.uncertain = True
+        return self
+    
+    
+    @property
+    def all_same_ac(self):
+        """Return True if all variants have same accession or no accession, otherwise False."""
+        num = len(set(var.ac for var in self.variants if var.ac))
+        return  num == 1 or num == 0 
+    
+    @property
+    def all_same_type(self):
+        """Return True if all variants have same type, otherwise False."""
+        return len(set(var.type for var in self.variants if var.type)) == 1
+    
+    @property
+    def ac(self):
+        if self.all_same_ac:
+            return self.variants[0].ac
+        else:
+            return [var.ac for var in self.variants]
+    
+    @property
+    def type(self):
+        return self.variants[0].type
+    
+    @property
+    def posedit(self):
+        posedits = [var.posedit for var in self.variants]
+        return hgvs.posedit.PosEditSet(posedits)
+
+
+
+class CompoundVariant(ComplexVariant):
+    """
+    represents a compound variant.
+    """
+    
+    def __init__(self, variants, phases, uncertain=False):
+        super(CompoundVariant, self).__init__(variants, phases, uncertain)
+        self.separator = ';'
+
+
+
+class MosaicVariant(ComplexVariant):
+    """
+    represents a compound variant.
+    """
+    
+    def __init__(self, variants, uncertain=False):
+        super(MosaicVariant, self).__init__(variants, None, uncertain)
+        self.separator = '/'
+
+
+
+class ChimericVariant(ComplexVariant):
+    """
+    represents a compound variant.
+    """
+    
+    def __init__(self, variants, uncertain=False):
+        super(ChimericVariant, self).__init__(variants, None, uncertain)
+        self.separator = '//'
+
+
 
 ## <LICENSE>
 ## Copyright 2014 HGVS Contributors (https://bitbucket.org/biocommons/hgvs)
