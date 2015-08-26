45da64c297bcb9983f402758588b46b81a2e1cff
Reece Hart <reecehart@gmail.com>
2015-08-26 14:19:41 -0700

    Backed out changeset: db46723a9407 Reason: Fails tests

--- hgvs/validator.py
+++ hgvs/validator.py
@@ -114,7 +114,7 @@ class ExtrinsicValidator():
         try:
             int(var_ref_seq)
             var_ref_seq = None
-        except ValueError:
+        except:
             pass
 
         if var_ref_seq is not None:
