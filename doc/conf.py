84d8de24cedea9bbaebd2a574f3ed79c44e57a69
Reece Hart <reecehart@gmail.com>
2015-12-04 16:43:35 -0800

    attempt to revert to invitae theme

--- doc/conf.py
+++ doc/conf.py
@@ -1,4 +1,18 @@
 ############################################################################
+## Theme setup
+
+html_theme = 'invitae'
+
+html_theme_path = ['themes']
+if html_theme == 'sphinx_rtd_theme':
+    import sphinx_rtd_theme
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+elif html_theme == 'bootstrap':
+    import sphinx_bootstrap_theme
+    html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+
+############################################################################
 ## Project config
 
 import hgvs
