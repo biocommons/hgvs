49e387fa39d6d0292c509f2cbef5ee080e7d922d
Reece Hart <reecehart@gmail.com>
2017-04-29 11:41:14 -0700

    added misc/refalt-repr-examples.py for #432, #431, #424

---
+++ misc/refalt-repr-examples.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import hgvs
+import hgvs.parser
+from tabulate import tabulate
+
+hp = hgvs.parser.Parser()
+
+variants = [
+    "NM_01.2:c.1_2del",
+    "NM_01.2:c.1_2del2",
+    "NM_01.2:c.1_2delAA",
+    "NM_01.2:c.1_2insTTT",
+    "NM_01.2:c.1_2delinsTTT",
+    "NM_01.2:c.1_2del2insTTT",
+    "NM_01.2:c.1_2delAAinsTTT",
+    "NM_01.2:c.1_2delAAinsAA",
+    "NM_01.2:c.1_1delAinsA",
+    "NM_01.2:c.1A>A",
+    "NM_01.2:c.1_1delAinsT",
+    "NM_01.2:c.1A>T",
+    "NM_01.2:c.1=",
+    "NM_01.2:c.1A=",
+    "NM_01.2:c.1AA=",
+]
+
+
+headers = "hgvs ref alt etype delta".split()
+def gen1(h):
+    v = hp.parse_hgvs_variant(h)
+    return [h, v.posedit.edit.ref, v.posedit.edit.alt, v.posedit.edit.type, v.posedit.length_change()]
+
+
+rows = [map(str, gen1(h)) for h in variants]
+
+print(tabulate(rows, headers=headers)) #, tablefmt="pipe"))
