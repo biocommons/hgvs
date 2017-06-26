b763d114dec475f00f39882e36066fd5d4747d4a
Reece Hart <reecehart@gmail.com>
2017-06-26 19:35:42 +0200

    split test issues by hundreds

--- tests/test_issues_03xx.py
+++ tests/test_issues_03xx.py
@@ -39,23 +39,6 @@ class Test_Issues(unittest.TestCase):
             self.hdp, replace_reference=True, assembly_name='GRCh38', alt_aln_method='splign')
         self.vn = hgvs.normalizer.Normalizer(self.hdp, shuffle_direction=3, cross_boundaries=True)
 
-    def test_260_raise_exception_when_mapping_bogus_variant(self):
-        v = self.hp.parse_hgvs_variant("NM_000059.3:c.7790delAAG")
-        with self.assertRaises(HGVSInvalidVariantError):
-            self.am37.c_to_p(v)
-
-    def test_285_partial_palindrome_inversion(self):
-        # https://github.com/biocommons/hgvs/issues/285/
-        # Inversion mapping is not palindrome aware
-        v = self.hp.parse_hgvs_variant("NM_000088.3:c.589_600inv")
-        vn = self.vn.normalize(v)
-        self.assertEqual(str(vn), "NM_000088.3:c.590_599inv")
-
-    def test_293_parser_attribute_assignment_error(self):
-        # https://github.com/biocommons/hgvs/issues/293/
-        var = self.hp.parse_hgvs_variant('NG_029146.1:g.6494delG')
-        self.vn.normalize(var)    # per issue, should raise error
-
     def test_307_validator_rejects_valid_interval(self):
         # https://github.com/biocommons/hgvs/issues/307/
         # before fix, raises error; after fix, should pass
