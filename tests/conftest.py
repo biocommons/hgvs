3aeb1e2a4bb12c65bfb25969daa0f1fb1831a471
Reece Hart <reecehart@gmail.com>
2018-09-04 23:48:23 -0700

    added tests to investigate issue 477; also add first set of fixtures in conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+import hgvs.easy
+
+
+@pytest.fixture(scope="session")
+def parser():
+    return hgvs.easy.parser
+
+@pytest.fixture(scope="session")
+def am38():
+    return hgvs.easy.am38
+
+
