c00107de4b0271f5235dc10538a002f2d81696bd
Reece Hart <reecehart@gmail.com>
2020-06-23 11:33:55 -0700

    #602: Added test to ensure no ref check for oob variant

---
+++ tests/issues/test_602.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import pytest
+
+import hgvs
+from hgvs.exceptions import HGVSInvalidIntervalError
+
+
+def test_602(parser, am37):
+    """ensure that there's no ref check for variants beyond bounds of transcript"""
+    
+    hgvs_g = "NC_000006.11:g.33415675_33422480del"
+    var_g = parser.parse(hgvs_g)
+
+    hgvs.global_config.mapping.strict_bounds = True   # default
+    with pytest.raises(HGVSInvalidIntervalError):
+        var_c = am37.g_to_c(var_g, "NM_006772.2")
+
+    hgvs.global_config.mapping.strict_bounds = False
+    var_c = am37.g_to_c(var_g, "NM_006772.2")  # No error
